9957890e9c887e19a6c04cf8f9c10db028d81384
Mateusz Boczarski <matbocz@outlook.com>
2020-08-08 17:50:57 +0200

    Install bootstrap, bootswatch and react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/flatly/bootstrap.min.css";
+
 import App from "./components/App";
 
 ReactDOM.render(
