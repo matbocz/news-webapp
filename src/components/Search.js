a3f09097e0ddfeceb1a6f777d12eaa67face6767
Mateusz Boczarski <matbocz@outlook.com>
2020-08-09 12:04:05 +0200

    Update Search.js layout

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FormControl } from "react-bootstrap";
 
 export default (props) => (
   <>
-    <input
+    <FormControl
       type="text"
+      placeholder="Search"
       value={props.search}
       onChange={(e) => props.setSearch(e.target.value)}
     />
