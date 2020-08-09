b0af76120385d7599dcea821755973d0e0127600
Mateusz Boczarski <matbocz@outlook.com>
2020-08-09 13:39:03 +0200

    Update PageSizeSelect layout

--- src/components/PageSizeSelect.js
+++ src/components/PageSizeSelect.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
 
 export default (props) => (
-  <>
-    <button onClick={() => props.setPageSize("5")}>5</button>
-    <button onClick={() => props.setPageSize("25")}>25</button>
-    <button onClick={() => props.setPageSize("50")}>50</button>
-    <button onClick={() => props.setPageSize("75")}>75</button>
-    <button onClick={() => props.setPageSize("100")}>100</button>
-  </>
+  <Row>
+    <Col className="d-flex justify-content-center">
+      <div>
+        <Button variant="outline-info" onClick={() => props.setPageSize("5")}>
+          5
+        </Button>{" "}
+        <Button variant="outline-info" onClick={() => props.setPageSize("25")}>
+          25
+        </Button>{" "}
+        <Button variant="outline-info" onClick={() => props.setPageSize("50")}>
+          50
+        </Button>{" "}
+        <Button variant="outline-info" onClick={() => props.setPageSize("75")}>
+          75
+        </Button>{" "}
+        <Button variant="outline-info" onClick={() => props.setPageSize("100")}>
+          100
+        </Button>
+      </div>
+    </Col>
+  </Row>
 );
