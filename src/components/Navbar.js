3f26d77066f9d5ce292ac34f81d40acf9ad50734
Mateusz Boczarski <matbocz@outlook.com>
2020-08-12 11:02:11 +0200

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,15 @@ import { Navbar, Form } from "react-bootstrap";
 import Search from "./Search";
 
 export default (props) => (
-  <>
-    <Navbar className="d-flex justify-content-between" bg="dark" variant="dark">
-      <Navbar.Brand>News</Navbar.Brand>
-      <Form inline>
-        <Search search={props.search} setSearch={props.setSearch} />
-      </Form>
-    </Navbar>
-  </>
+  <Navbar
+    className="d-flex justify-content-between"
+    style={{ minHeight: "8vh" }}
+    bg="dark"
+    variant="dark"
+  >
+    <Navbar.Brand>News</Navbar.Brand>
+    <Form inline>
+      <Search search={props.search} setSearch={props.setSearch} />
+    </Form>
+  </Navbar>
 );
