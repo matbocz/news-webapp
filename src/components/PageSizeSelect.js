0f221cf2932bac678b0903ef868bd11522e2f910
Mateusz Boczarski <matbocz@outlook.com>
2020-08-12 14:21:37 +0200

    Update PageSizeSelect

--- src/components/PageSizeSelect.js
+++ src/components/PageSizeSelect.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { Row, Col, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export default (props) => (
-  <Row>
-    <Col className="d-flex justify-content-center">
-      <div>
-        <Button variant="outline-info" onClick={() => props.setPageSize("5")}>
-          5
-        </Button>{" "}
-        <Button variant="outline-info" onClick={() => props.setPageSize("25")}>
-          25
-        </Button>{" "}
-        <Button variant="outline-info" onClick={() => props.setPageSize("50")}>
-          50
-        </Button>{" "}
-        <Button variant="outline-info" onClick={() => props.setPageSize("75")}>
-          75
-        </Button>{" "}
-        <Button variant="outline-info" onClick={() => props.setPageSize("100")}>
-          100
-        </Button>
-      </div>
-    </Col>
-  </Row>
+  <div className="d-flex justify-content-end">
+    <div>
+      <Button variant="outline-info" onClick={() => props.setPageSize("5")}>
+        5
+      </Button>{" "}
+      <Button variant="outline-info" onClick={() => props.setPageSize("25")}>
+        25
+      </Button>{" "}
+      <Button variant="outline-info" onClick={() => props.setPageSize("50")}>
+        50
+      </Button>{" "}
+      <Button variant="outline-info" onClick={() => props.setPageSize("75")}>
+        75
+      </Button>{" "}
+      <Button variant="outline-info" onClick={() => props.setPageSize("100")}>
+        100
+      </Button>
+    </div>
+  </div>
 );
