aa23b6fa746d64202b133d74aa04b46fe561363b
Mateusz Boczarski <matbocz@outlook.com>
2020-08-15 15:54:49 +0200

    Create main app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+
+import ArticlesList from "./ArticlesList";
+import PageSizeSelect from "./PageSizeSelect";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import FilterMenu from "./FilterMenu";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [lang, setLang] = useState("en");
+  const [pageSize, setPageSize] = useState("25");
+  const [search, setSearch] = useState("tech");
+  const [sortMethod, setSortMethod] = useState("relevancy");
+
+  useEffect(() => {
+    if (search.length === 0) {
+      setData([]);
+    } else {
+      const API_KEY = process.env.REACT_APP_API_KEY;
+
+      fetch(
+        `https://newsapi.org/v2/everything?q=${search}&language=${lang}&pageSize=${pageSize}&sortBy=${sortMethod}&apiKey=${API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => setData(data.articles));
+    }
+  }, [search, lang, pageSize, sortMethod]);
+
   return (
     <>
-      <p>News-WebApp</p>
+      <Navbar search={search} setSearch={setSearch} />
+
+      <Container>
+        <FilterMenu
+          lang={lang}
+          setLang={setLang}
+          sortMethod={sortMethod}
+          setSortMethod={setSortMethod}
+        />
+
+        <ArticlesList articles={data} />
+
+        <PageSizeSelect setPageSize={setPageSize} />
+      </Container>
+
+      <Footer />
     </>
   );
 }
